7020096b8942423b8ba88b2a0ba585e3fbc84433
Sithvertex <ibusu@yandex.ru>
2022-10-14 16:23:29 +0300

    Add_second task

--- Beging/HM_1/Task_1.py
+++ Beging/HM_1/Task_1.py
@@ -1,1 +1,8 @@
-print('Hello, World')
+a = int(input("Enter the day of the week (number): "))
+if a==6 or a==7:
+    print("weekend")
+elif 0 > a < 8:
+    print("It's not a day of the week")
+else:
+    print("weekday")
+print("Thank you for participating in the survey:)")
