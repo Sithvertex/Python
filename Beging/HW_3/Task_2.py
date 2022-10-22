b9f85efa2370d65d88ef755edf81e6f16c6c8179
Roman <111444518+Sithvertex@users.noreply.github.com>
2022-10-22 14:02:57 +0300

    Update Task_2.py
    Уточнил Task_2

--- Beging/HW_3/Task_2.py
+++ Beging/HW_3/Task_2.py
@@ -4,8 +4,12 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-data = [2, 3, 4, 5, 6]
+data = [2, 3, 5, 6]
 rn_list = []
-for i in range(0, len(data) // 2):
+if (len(data) % 2) == 0:
+    N = len(data) // 2
+else:
+    N = len(data) // 2 + 1
+for i in range(0, N):
     rn_list.append(data[i] * data[(len(data)-1)-i])
 print(rn_list)
